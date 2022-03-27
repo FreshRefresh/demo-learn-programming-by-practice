e85d89b0a7a0c9ffa6040e5faf981a0ccdf35ef9
Ram Mittala <rmittala@godaddy.com>
2022-03-26 19:54:40 -0700

    Initial commit - Project setup

---
+++ src/main/java/com/ram/demolearnprogrammingbypractice/DemoLearnProgrammingByPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ram.demolearnprogrammingbypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLearnProgrammingByPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoLearnProgrammingByPracticeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ram/demolearnprogrammingbypractice/DemoLearnProgrammingByPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.demolearnprogrammingbypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLearnProgrammingByPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
